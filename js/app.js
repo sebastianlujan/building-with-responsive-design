9cb63d10fdb997dd01225765ba35da16b8c34e0e
Sebastian Lujan <glujan.recalde@gmail.com>
2019-09-30 14:27:58 -0300

    added show button

---
+++ js/app.js
@@ -0,0 +1,11 @@
+  
+show = () => {
+  var menu = document.getElementById('nav-tog');
+  menu.style.display = 'block';
+  menu.style.height = '100%';
+  console.log("yayra")
+
+  var close = document.getElementById('close-button');
+  menu.style.display = 'none';
+
+}
