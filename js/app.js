a0001c1eabaa64178fd0d3f20a2667b03ce25b10
Eduardo Bonilla <eduardobonillac@gmail.com>
2019-10-01 15:43:53 -0500

    breakpoint corrections

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 
 show = () => {
   var menu = document.getElementById('nav-tog');
-
-  if(menu.style.display=="none")
     menu.style.display = 'block';
+}
 
-  else
-    menu.style.display = 'none';
-
+hide = () => {
+  var hid = document.getElementById('nav-tog')
+    hid.style.display = 'none';
 }
